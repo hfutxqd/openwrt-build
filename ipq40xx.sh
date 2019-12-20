84c418e8cde2187d0569c3e8a8e05bce1c096b77
xuqd <xuqd@getui.com>
2019-12-20 10:48:10 +0800

    test

--- ipq40xx.sh
+++ ipq40xx.sh
@@ -2,7 +2,7 @@
 
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
-make defconfig
-sed -i 's/# CONFIG_ALL .*/CONFIG_ALL=y/' .config
-sed -i 's/# CONFIG_TARGET_ipq40xx .*/CONFIG_TARGET_ipq40xx=y/' .config
-sed -i 's/# CONFIG_TARGET_ipq40xx_Default .*/CONFIG_TARGET_ipq40xx_Default=y/' .config
+touch .config
+echo 'CONFIG_ALL=y' >> .config
+echo 'CONFIG_TARGET_ipq40xx=y' >> .config
+echo 'CONFIG_TARGET_ipq40xx_Default=y' >> .config
